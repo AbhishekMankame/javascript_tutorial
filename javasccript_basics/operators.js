c81aa3626234ec6e96c9093fdba64ed779e377ec
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-09 23:31:49 +0530

    bmi_john_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -56,3 +56,4 @@ const heightJohn = 1.95;
 // BMI = mass / height ** 2 = mass / (height * height)
 
 const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
