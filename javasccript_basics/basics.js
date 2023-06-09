752b1ad07c151593cd83795133ed8a4c60c52039
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-09 09:12:18 +0530

    comment_update

--- javasccript_basics/basics.js
+++ javasccript_basics/basics.js
@@ -11,5 +11,6 @@ console.log(firstName);
 
 //let a& b = 'AB'; --> This one gives syntax error
 // only $ and _ are the special symnols which are allowed in JavaScript
+// variables name should not start with uppercase letters
 
 
