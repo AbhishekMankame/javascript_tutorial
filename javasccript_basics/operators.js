4bb834cb81865b46d334525efcd95b3b56342260
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-09 22:06:00 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -3,3 +3,6 @@ const now = 2037
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+console.log(ageJonas * 2);
