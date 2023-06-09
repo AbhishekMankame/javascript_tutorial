4370cd0be997e723906aab9c361457c3742bf24f
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-09 22:09:54 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -5,5 +5,7 @@ const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
 console.log(ageJonas * 2);
 console.log(ageJonas / 5, ageJonas * 3);
