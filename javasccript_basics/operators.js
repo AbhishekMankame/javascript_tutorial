21736d9dda49cad78c743f8255f0e63ad63e73a5
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-09 22:16:40 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -14,3 +14,6 @@ const firstName2 = 'Abhishek';
 const lastName = 'Mankame';
 console.log(firstName2 + lastName);
 console.log(firstName2 + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
