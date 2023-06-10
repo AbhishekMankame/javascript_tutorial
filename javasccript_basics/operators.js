a038e6f4e75ef546a5390fa234925713838586cc
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-10 13:05:25 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -149,4 +149,5 @@ if (BMIMark > BMIJohn) {
     console.log(`Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})!`);
 } else {
     console.log("John's BMI is higher than Mark's BMI");
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})!`);
 }
