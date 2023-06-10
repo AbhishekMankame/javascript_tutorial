ab8882631f2223df414612c611de4bb8c9a6f93e
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-10 13:00:18 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -126,10 +126,16 @@ else {
 console.log(century);
 
 // Coding Challenge #2
-const massMark = 95;
-const heightMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
 
 // Calcualting the BMI of Mark and John
 // BMI = mass / height ** 2 = mass / (height * height)
