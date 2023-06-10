62f27f084bd4a73d15e25a080687446decfac442
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-10 23:56:41 +0530

    falsy_truthy_values_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -189,3 +189,10 @@ console.log('10' - '4' - '3' - 2 + '5'); // output: 15
 
 // Truthy and Falsy Values
 // 5 falsy values in javascript are: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(1));
+console.log(Boolean(''));
+console.log(Boolean('JavaScript'));
+console.log(Boolean(0));
