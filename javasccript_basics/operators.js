95fad2edea77e6add8fd713e8864bfc7704b0b96
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-13 11:24:08 +0530

    simple_update

--- javasccript_basics/operators.js
+++ javasccript_basics/operators.js
@@ -409,3 +409,4 @@ console.log(drink2);
 console.log(`I like to drink ${age4 >= 18 ? 'wine 🍷' : 'water 💧'}`);
 
 // Ternary operator is not at all a replacement for an if else statement, we still need if else statement all the time
+// Ternary operator is perfect when we need to take a quick decision based on a condition
