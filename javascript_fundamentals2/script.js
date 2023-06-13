dfa9b7c452afb98d855e25fbc05c5aed208104ad
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-13 13:44:37 +0530

    simple_comment

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -1,2 +1,2 @@
 // strict mode is a special mode which makes us to write secure JavaScript code
-'use strict';   // this statement should be the very first statement in the script
+'use strict';   // this statement should be the very first statement in the script, if there is any code before this statement, it will not work in strict mode
