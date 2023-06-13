90a69a22cbbbf7dc7c71696ad6ae69e47c56c328
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-13 13:51:09 +0530

    simple_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -1,3 +1,8 @@
 // strict mode is a special mode which makes us to write secure JavaScript code
 'use strict';   // this statement should be the very first statement in the script, if there is any code before this statement, it will not work in strict mode
 // strict mode is used to avoid accidental errors in the code
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // this will not throw any error in normal mode, but in strict mode it will throw an error
