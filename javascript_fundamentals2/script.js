600183e18c07429caa47a394968d2526d6cbca0b
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-16 06:53:53 +0530

    simple_comment_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -1,11 +1,27 @@
-// strict mode is a special mode which makes us to write secure JavaScript code
-//'use strict';   // this statement should be the very first statement in the script, if there is any code before this statement, it will not work in strict mode
-// strict mode is used to avoid accidental errors in the code
+// // strict mode is a special mode which makes us to write secure JavaScript code
+// //'use strict';   // this statement should be the very first statement in the script, if there is any code before this statement, it will not work in strict mode
+// // strict mode is used to avoid accidental errors in the code
 
-let hasDriversLicense = false;
-const passTest = true;
+// // 1. Declare a variable called 'hasDriversLicense' using the 'let' keyword, and set it to false
+// let hasDriversLicense = false;
 
-if (passTest) hasDriverLicense = true; // this will not throw any error in normal mode, but in strict mode it will throw an error
-if (hasDriversLicense) console.log('I can drive :D');
+// // 2. Declare a variable called 'passTest' using the 'const' keyword, and set it to true
+// const passTest = true;
 
-// alt + closing bracket will be used to toggle the comment in the copilot
+// // 3. Create an if statement with the condition 'passTest' that sets the value of 'hasDriversLicense' to true
+// if (passTest) hasDriversLicense = true;
+
+// // 4. Create an if statement with the condition 'hasDriversLicense' that logs 'I can drive :D' to the console
+// if (hasDriversLicense) console.log('I can drive :D');
+
+// // alt + closing bracket will be used to toggle the comment in the copilot
+
+// // 5. Change the value of 'passTest' to false
+
+// // linear search in javascript
+// function linearSearch(arr, val) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === val) return i;
+//     }
+//     return -1;
+// }
