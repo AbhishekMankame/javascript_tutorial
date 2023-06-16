44dc473af832e347e6deb3168725eb42ab38a174
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-16 21:07:48 +0530

    error_resolve_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -115,6 +115,6 @@ console.log(age3);
 const yearsUntilRetirement = (birthYear, firstName) => {
     const age = 2037 - birthYear;
     const retirement = 65 - age;
-    return `${firstName} retires in {retirement} years.`;
+    return `${firstName} retires in ${retirement} years.`;
 }
 console.log(yearsUntilRetirement(1991, 'Jonas'));
