a6a0caf9d6b732b7a077ea3e84f3410913ab7a9f
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-17 14:45:57 +0530

    simple_data_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -183,7 +183,7 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 checkWinner(scoreDolphins, scoreKoalas);
 
 // test 2
-scoreDolphins = calcAverage(44, 23, 71);
-scoreKoalas = calcAverage(65, 54, 49);
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
