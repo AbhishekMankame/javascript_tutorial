84c0fb248c641945332d012baf7d9e0dbea8334b
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-17 10:12:26 +0530

    output_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -134,3 +134,5 @@ function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
     return juice;
 }
+
+console.log(fruitProcessor(2, 3));
