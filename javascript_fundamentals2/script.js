b12db921f64ed13adbc0af008bc6fa03f9b9036d
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-18 12:29:26 +0530

    conditiional_statement_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -257,3 +257,7 @@ console.log(friends.includes('Steven')); // includes method returns true if the 
 
 console.log(friends.includes('Bob')); // includes function works with strict equality
 // we can write incoudes method to write conditional statements
+
+if (friends.includes('Peter')) {
+    console.log('You have a friend called Peter');
+}
