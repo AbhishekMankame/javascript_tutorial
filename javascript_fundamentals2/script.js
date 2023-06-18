f674eee2b24345fae8f135177140cdaebe3bd6b4
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-18 12:40:51 +0530

    arrow_function_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -266,3 +266,5 @@ if (friends.includes('Steven')) {
 const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
+
+// const calcTip => bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
