7e7093967fb45b3ac101cd21685985e0a3ff4d97
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-18 13:04:28 +0530

    simple_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -265,7 +265,7 @@ if (friends.includes('Steven')) {
 // Coding Exercise 6: CHALLENGE #2
 const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+};
 
 // const calcTip => bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
