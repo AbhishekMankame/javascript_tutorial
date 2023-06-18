0b84a1832a5d2b1017ad5347a69be962474d73c1
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-18 12:27:45 +0530

    includes_with_usecase_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -256,3 +256,4 @@ console.log(friends.indexOf('Bob')); // returns -1 if the element is not in the 
 console.log(friends.includes('Steven')); // includes method returns true if the element is in the array, otherwise returns false
 
 console.log(friends.includes('Bob')); // includes function works with strict equality
+// we can write incoudes method to write conditional statements
