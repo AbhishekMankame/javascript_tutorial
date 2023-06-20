c509be7db0cb858399d1a9a358d50ee027f4b51a
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-20 21:49:55 +0530

    simple_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -437,7 +437,9 @@ for (let i = 0; i < jonasArray.length; i++) {
     // reading from jonasArray
     console.log(jonasArray[i], typeof jonasArray[i]);
     // filling types array
-    types[i] = typeof jonasArray[i];
+
+    //types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
 }
 
 console.log(types);
