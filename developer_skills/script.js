d76cf32bed4b1b4412efa08babe86170ceec52a4
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 17:52:37 +0530

    simple_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -25,3 +25,5 @@ console.log('This is loaded through the help of live server');
 // Using Google, StackOverflow and MDN
 // PROBLEM:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
