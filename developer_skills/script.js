f82c3eafdfdcc8e55a9eda103b245988b426a5e8
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 13:28:55 +0530

    simple_configuration_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -1,3 +1,3 @@
 // Remember, we're gonna use strict mode in all scripts now!S
-"use strict";
+'use strict';
 // Prettier: It is an opinionated code formatter, which means that it makes assumptions about how good code should look like, and so if we install it and use it, it will automatically format our code according to these assumptions.
