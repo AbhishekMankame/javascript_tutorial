e6ad6522face76294c2c9fbe9f424b1c7f0a5d6e
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 16:56:34 +0530

    basic_steps

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -12,4 +12,6 @@ console.log('This is loaded through the help of live server');
 // Learning how to code
 
 // How to think like a developer: Become a problem solver
+// Fixing the problem solving basic issue
+// Take a very logical and rational approach to solving problems (programming is just logic)
 // John's 5 steps to solve any coding problem
