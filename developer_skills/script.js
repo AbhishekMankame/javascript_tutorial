65814026127cafc89df976cad152a3bfde6be41a
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 20:30:53 +0530

    output_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -44,9 +44,12 @@ const calcTempAmplitude = function (temps) {
   let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
     const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
   }
   console.log(max, min);
 };
 calcTempAmplitude([3, 5, 6]);
+calcTempAmplitude(temperature);
