c6ddd75633bcde6564dd468a56c43b56ea0529f2
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 13:37:34 +0530

    simple_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -1,3 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!S
 'use strict';
 // Prettier: It is an opinionated code formatter, which means that it makes assumptions about how good code should look like, and so if we install it and use it, it will automatically format our code according to these assumptions.
+
+const x = '23';
+if (x === 23) console.log(23);
