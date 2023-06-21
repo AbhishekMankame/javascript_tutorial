0001c6f26c3f872aa0fcd55cd5d7fb18e8f72098
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 17:54:24 +0530

    steps_to_solve

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -27,3 +27,7 @@ console.log('This is loaded through the help of live server');
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1. Understanding the problem
+
+// 2. Breaking up into sub-problem
