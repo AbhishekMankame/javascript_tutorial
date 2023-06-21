1f5d45ca47c4ce38d105ae032360c4f9c8f54684
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 11:09:41 +0530

    inner_loop_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -475,4 +475,7 @@ for (let i = jonasArray.length - 1; i >= 0; i--) {
 // Looping inside a Loop
 for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`-------- Starting exercise ${exercise}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Lifting weight repetition ${rep} 🏋️‍♀️`);
+    }
 }
