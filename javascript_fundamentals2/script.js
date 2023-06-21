db510c8f4126a0999500d0c94985d1a4b3139aa2
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-21 11:11:40 +0530

    output_update

--- javascript_fundamentals2/script.js
+++ javascript_fundamentals2/script.js
@@ -476,6 +476,6 @@ for (let i = jonasArray.length - 1; i >= 0; i--) {
 for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`-------- Starting exercise ${exercise}`);
     for (let rep = 1; rep < 6; rep++) {
-        console.log(`Lifting weight repetition ${rep} 🏋️‍♀️`);
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
     }
 }
