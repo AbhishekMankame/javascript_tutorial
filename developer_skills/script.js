4c213107d4cf30c6fd47f8c295d328e28d2fbf87
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-23 19:37:11 +0530

    simple_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -130,4 +130,7 @@ console.log(measureKelvin());
 Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
 
 Create a function 'printforecast' which takes in an array 'arr' and logs a string like the above to the console. Use the problem-solving framework: Understand the problem and break it up into sub-problems! 
+
+TEST DATA 1: [17, 27, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
 */
