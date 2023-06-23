bfea1e460cf58ca899e032695d6298ea0b6d5150
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-23 21:08:58 +0530

    simple_update

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -156,7 +156,7 @@ const printForecast = function (arr) {
   for (let i = 0; i < arr.length; i++) {
     str += `${arr[i]}ºC in ${i + 1} days ... `;
   }
-  console.log(str);
+  console.log('... ' + str);
 };
 
 printForecast(data1);
