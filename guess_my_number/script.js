714b8d4aaf4147ae0927dbcd400b3157404e030c
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-27 22:00:36 +0530

    simple_update

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -16,3 +16,5 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number! 🎉';
 // console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
