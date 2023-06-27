6d5dbfce74874be95a08fabc2c68f9602acef5e3
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-27 21:14:15 +0530

    simple_update

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -5,4 +5,4 @@ console.log(document.querySelector('.message').textContent);
 // Document object model (DOM)
 // Document Object Model(DOM): Structured representation of HTML documents. Allows javascript to access html elements and styles to manipulate then
 // DOM is basically a connection point between HTML and Javascript
-// DOM is like a tree structure of all the elements in the html document
+// DOM is like a tree structure of all the elements in the html document. here each element is an object
