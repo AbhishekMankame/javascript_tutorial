ae25fcbc2116cffb52d51b04c90dabe313f91224
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-29 18:08:53 +0530

    simple_update

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -18,7 +18,6 @@
 // // console.log(document.querySelector('.message').textContent);
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
-
 // console.log(document.querySelector('.guess').value);
 
 'use strict';
