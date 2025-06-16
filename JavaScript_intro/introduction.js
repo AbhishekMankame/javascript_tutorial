e7675ba5d953ca2e1df61e9660e88f419bc49248
abhishek <abhishekmankame962@gmail.com>
2025-06-16 21:01:26 +0530

    JS introduction

---
+++ JavaScript_intro/introduction.js
@@ -0,0 +1,3 @@
+// JavaScript is a high-level, object-oriented, multi-paradigm programming language.
+// It is widely used for web development, both on the client-side and server-side.
+// JavaScript is an interpreted language, meaning it is executed line by line at runtime.
