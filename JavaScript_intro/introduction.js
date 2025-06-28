fafb0c47a6ab341bb40c6b55f64f3a43fda767c7
abhishek <abhishekmankame962@gmail.com>
2025-06-28 13:46:28 +0530

    simple html code update

--- JavaScript_intro/introduction.js
+++ JavaScript_intro/introduction.js
@@ -1,10 +1,16 @@
 // JavaScript is a high-level, object-oriented, multi-paradigm programming language.
 // It is widely used for web development, both on the client-side and server-side.
+
 // JavaScript is an interpreted language, meaning it is executed line by line at runtime.
 
 // Here programming language --> means it insctructs computer to do things.
+
 // High-level language --> means it is closer to human language and easier to read and write. Herr we don't have to worry about complex stuff like memory management.
+
 // Object-oriented --> means it is based on objects that can contain data and functions. Based on ohects, for storing most kinds of data.
+
 // Multi-paradigm --> means it supports different, flexible programming styles, such as procedural, functional, and object-oriented programming.
+
 // Interpreted language --> means it is executed by an interpreter, which reads and executes the code line by line at runtime.
+
 // JavaScript is a versatile language that can be used for various purposes, including web development, server-side programming, game development, and more.
