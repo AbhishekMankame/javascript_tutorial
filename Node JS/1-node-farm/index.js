c1e87268ecc1648e07c45a55a0975978789aeba7
abhishek <abhishekmankame962@gmail.com>
2025-08-02 23:36:25 +0530

    async example update

--- Node JS/1-node-farm/index.js
+++ Node JS/1-node-farm/index.js
@@ -14,6 +14,10 @@ const fs = require('fs'); // Here fs --> file system
 fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
     fs.readFile(`./starter/txt/${data1}.txt`,'utf-8', (err,data2) => {
         console.log(data2);
+        fs.readFile(`./starter/txt/append.txt`,'utf-8', (err,data3) => {
+        console.log(data3);
+
+    });
     });
 
 
