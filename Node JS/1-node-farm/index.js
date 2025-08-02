ea089795b2f984c6cce7ec7f9512bfd797d3092a
abhishek <abhishekmankame962@gmail.com>
2025-08-02 23:32:30 +0530

    async code update

--- Node JS/1-node-farm/index.js
+++ Node JS/1-node-farm/index.js
@@ -1,16 +1,23 @@
 const fs = require('fs'); // Here fs --> file system
 
-const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8');
-console.log(textIn);
+// const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8');
+// console.log(textIn);
 
 // Blocking, synchronous way
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./starter/txt/output.txt', textOut);
-console.log('File written!');
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// console.log('File written!');
 // const hello = 'Hello World';
 // console.log(hello);
 
 // Non-blocking, asynchrous way
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./starter/txt/${data1}.txt`,'utf-8', (err,data2) => {
+        console.log(data2);
+    });
+
+
+
+    // console.log(data); // Note: Here text also works file instead of data
 });
+console.log('Will read file!');
