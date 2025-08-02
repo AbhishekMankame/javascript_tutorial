ef650b31c7bb0d669e4b337209b4b0ab7e3f7b02
abhishek <abhishekmankame962@gmail.com>
2025-08-02 12:15:15 +0530

    io-read-write update

--- Node JS/1-node-farm/index.js
+++ Node JS/1-node-farm/index.js
@@ -1,2 +1,10 @@
-const hello = 'Hello World';
-console.log(hello);
+const fs = require('fs'); // Here fs --> file system
+
+const textIn = fs.readFileSync('./starter/txt/input.txt','utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./starter/txt/output.txt', textOut);
+console.log('File written!');
+// const hello = 'Hello World';
+// console.log(hello);
