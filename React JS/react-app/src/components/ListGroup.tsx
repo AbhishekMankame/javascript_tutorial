3c112c1658987e98e9f6be4057340534c2afc56a
abhishek <abhishekmankame962@gmail.com>
2025-08-06 23:49:11 +0530

    list group fragment update

--- React JS/react-app/src/components/ListGroup.tsx
+++ React JS/react-app/src/components/ListGroup.tsx
@@ -1,13 +1,29 @@
+import { Fragment } from "react";
+
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length == 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
 export default ListGroup;
+
+// Note: In react it is not allowed to return multiple elements
+//Compiled to JS as React.createElement('h1')
