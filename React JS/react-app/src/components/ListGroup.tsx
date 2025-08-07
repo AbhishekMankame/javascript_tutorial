bcb5307e2c7b1eebabeeb69a13752f90f2bfaf64
abhishek <abhishekmankame962@gmail.com>
2025-08-07 23:12:50 +0530

    simple update

--- React JS/react-app/src/components/ListGroup.tsx
+++ React JS/react-app/src/components/ListGroup.tsx
@@ -4,16 +4,13 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  if (items.length == 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
   return (
     <Fragment>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
