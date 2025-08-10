7071fecb4bbc9a548e20c37ef3160cf19fa81155
abhishek <abhishekmankame962@gmail.com>
2025-08-11 02:23:32 +0530

    variables in js

---
+++ JavaScript_intro/hitesh_sir/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553 // const ka matlab yahi hota hai ki, hum usko change nahi kiya jaa sakta
+let accountEmail = "abcd@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 // Not allowed --> It given error
+console.log(accountId);
+
+console.log(accountEmail)
+accountEmail = "defg@google.com"
+console.log(accountEmail)
