ceb9b5fcf79073e83094f27608f8d43576e938b7
abhishek <abhishekmankame962@gmail.com>
2025-08-11 02:31:36 +0530

    undefined behaviour

--- JavaScript_intro/hitesh_sir/variables.js
+++ JavaScript_intro/hitesh_sir/variables.js
@@ -1,7 +1,8 @@
 const accountId = 144553 // const ka matlab yahi hota hai ki, hum usko change nahi kiya jaa sakta
 let accountEmail = "abcd@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" // This is not a good method
+let accountState;
 
 //accountId = 2 // Not allowed --> It given error
 console.log(accountId);
@@ -18,4 +19,10 @@ console.log(accountCity)
 accountCity = "Bengaluru"
 console.log(accountCity)
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountState)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+ Prefer not to use var --> Because of issue in block scope and functional scope
+*/
