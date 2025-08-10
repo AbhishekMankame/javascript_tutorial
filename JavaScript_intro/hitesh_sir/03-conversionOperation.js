83d1e4bc3d1f7a4e4014f1ac3eba59f92ce750bc
abhishek <abhishekmankame962@gmail.com>
2025-08-11 03:08:07 +0530

    bool to int

--- JavaScript_intro/hitesh_sir/03-conversionOperation.js
+++ JavaScript_intro/hitesh_sir/03-conversionOperation.js
@@ -7,3 +7,16 @@ console.log(typeof score)
 let valueInNumber = Number(score)
 console.log(valueInNumber)
 console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a number)
+// trye => 1
+// false => 0
+
+let isLoggedIn =1
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let booleanIsLoggenIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggenIn)
+console.log(typeof booleanIsLoggenIn)
