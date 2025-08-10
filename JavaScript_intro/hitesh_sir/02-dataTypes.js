d3ff585ee8fa6f725dd094c00d650a6311b5643a
abhishek <abhishekmankame962@gmail.com>
2025-08-11 02:46:04 +0530

    code readability update

---
+++ JavaScript_intro/hitesh_sir/02-dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // We are using node.js, not browser
+
+// console.log(3+3); console.log("Abc")  --> This is not a good practice, code is not readable
