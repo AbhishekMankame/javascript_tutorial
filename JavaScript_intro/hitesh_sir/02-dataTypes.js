c37dedf87b79a3fc19f525d9049993406cf50983
abhishek <abhishekmankame962@gmail.com>
2025-08-11 02:53:26 +0530

    data types in JS

--- JavaScript_intro/hitesh_sir/02-dataTypes.js
+++ JavaScript_intro/hitesh_sir/02-dataTypes.js
@@ -3,3 +3,22 @@
 // alert("Hello") // We are using node.js, not browser
 
 // console.log(3+3); console.log("Abc")  --> This is not a good practice, code is not readable
+// Code readability should be high
+
+let name = "hitesh" // string data type
+let age = 30 // integer data type
+let isLoggenIn = false // boolean data type
+let state = null
+
+// number => 2 to the power 53 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  (null is a representation of empty value)
+// undefined => abhi value assign nahi hui hai
+// symbol => unique ke liye
+
+// object
+
+console.log(typeof "hitest")
+console.log(typeof age)
