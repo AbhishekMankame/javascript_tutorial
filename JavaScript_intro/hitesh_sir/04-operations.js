7520852ce5becf1cc97dd0a726c6cb326734c330
abhishek <abhishekmankame962@gmail.com>
2025-08-12 00:05:55 +0530

    increment operator update

--- JavaScript_intro/hitesh_sir/04-operations.js
+++ JavaScript_intro/hitesh_sir/04-operations.js
@@ -19,8 +19,21 @@ let str2 = " mankame"
 let str3 = str1 + str2
 console.log(str3)
 
-console.log(1+"2")
-console.log("1"+2)
-console.log("1"+2+2)
-console.log(1+2+"2")
-console.log("1"+(2+2))
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log("1"+(2+2))
+
+console.log(+true) // not a good method
+console.log(+false)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  // not a good method, bad for readability
+console.log([num1, num2, num3])
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
