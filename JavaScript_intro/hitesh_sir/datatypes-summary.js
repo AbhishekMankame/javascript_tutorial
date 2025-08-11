4acfbb2ceef18d8f7260bbad9d2e4f234ca3080b
abhishek <abhishekmankame962@gmail.com>
2025-08-12 00:32:32 +0530

    data types summary start

---
+++ JavaScript_intro/hitesh_sir/datatypes-summary.js
@@ -0,0 +1,5 @@
+// datatypes summary
+// kis tarah se data ko memory mei rakha jaata hai and access kiya jaata hai, uss basis pe data ka 2 categorization hai
+
+// # Primitive datatypes --> Yeh call by value hote hai
+// 7 types: String, Number, Boolean, null, undefined, Symbol(kisi bhi value ko unique banane keliye symbol ka use hota hai), 
