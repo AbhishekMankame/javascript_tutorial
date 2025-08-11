723cd6b58a50958839e60be3a9e703bce9edeff0
abhishek <abhishekmankame962@gmail.com>
2025-08-11 23:58:23 +0530

    string concatenation

--- JavaScript_intro/hitesh_sir/04-operations.js
+++ JavaScript_intro/hitesh_sir/04-operations.js
@@ -5,10 +5,16 @@ let value = 3
 let negValue = -value
 // console.log(negValue)
 
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2/2)
-console.log(2/0)
-console.log(2%2)
-console.log(2**2)
+console.log(2+2)   // addition
+console.log(2-2)   // subtraction
+console.log(2*2)   // multiplication
+console.log(2/2)   // division
+console.log(2/0)   // divide by 0
+console.log(2%2)   // remainder
+console.log(2**2)  // power (exponent)
+
+let str1 = "abhishek"
+let str2 = " mankame"
+
+let str3 = str1 + str2
+console.log(str3)
