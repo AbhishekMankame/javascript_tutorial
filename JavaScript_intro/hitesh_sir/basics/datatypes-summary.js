1283d8504ad3287082a9765e6511a718b9271294
abhishek <abhishekmankame962@gmail.com>
2025-08-12 21:32:56 +0530

    pass by reference

--- JavaScript_intro/hitesh_sir/basics/datatypes-summary.js
+++ JavaScript_intro/hitesh_sir/basics/datatypes-summary.js
@@ -57,3 +57,20 @@ let anotherName = myYoutubename
 anotherName = "Chaiaurcode"
 console.log(myYoutubename)
 console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userOne)
+console.log(userTwo)
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne)
+console.log(userTwo)
+console.log(userOne.email)
+console.log(userTwo.email)
