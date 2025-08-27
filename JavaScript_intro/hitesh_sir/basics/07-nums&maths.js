4fecea631a0c1379edf809fa499668a2cfb67245
abhishek <abhishekmankame962@gmail.com>
2025-08-27 23:02:39 +0530

    precision value

--- JavaScript_intro/hitesh_sir/basics/07-nums&maths.js
+++ JavaScript_intro/hitesh_sir/basics/07-nums&maths.js
@@ -7,3 +7,6 @@ console.log(balance);
 console.log(balance.toString());
 console.log(balance.toString().length);
 console.log(balance.toFixed(3));
+
+const otherNumber = 23.8966;
+console.log(otherNumber.toPrecision(15));
