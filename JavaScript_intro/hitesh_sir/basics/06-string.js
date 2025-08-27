1753981a5c6f8d08a853d8780620d351a18cc748
abhishek <abhishekmankame962@gmail.com>
2025-08-27 22:21:16 +0530

    some more functions update

--- JavaScript_intro/hitesh_sir/basics/06-string.js
+++ JavaScript_intro/hitesh_sir/basics/06-string.js
@@ -7,10 +7,19 @@ console.log(name + repoCount)
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) // String interpolation
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc')
+// String methods
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase()); // yaha par original value change nahi hota
 console.log(gameName);
 console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4); // here 0th character is included and 4th character is not included
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
