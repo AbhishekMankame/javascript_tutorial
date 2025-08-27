5b4493e9155394d556559a4d154bab7bbefcfc7a
abhishek <abhishekmankame962@gmail.com>
2025-08-27 22:06:24 +0530

    strings update

--- JavaScript_intro/hitesh_sir/basics/06-string.js
+++ JavaScript_intro/hitesh_sir/basics/06-string.js
@@ -9,3 +9,5 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) // Str
 
 const gameName = new String('hiteshhc')
 console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
