165af1093688dadd32da8b532ec27b586751b039
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:36:01 +0530

    date&time

--- JavaScript_intro/hitesh_sir/basics/08_dates_in_js.js
+++ JavaScript_intro/hitesh_sir/basics/08_dates_in_js.js
@@ -17,3 +17,21 @@ console.log(myCreatedDate2.toLocaleString());
 let myCreatedDate3 = new Date("11-21-2023"); //MM-DD-YY format
 console.log(myCreatedDate3);
 console.log(myCreatedDate3.toLocaleString());
+
+// Time stamps
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time is`
+newDate.toLocaleString('default',{
+    weekday: "long",
+    timeZone: ''
+})
