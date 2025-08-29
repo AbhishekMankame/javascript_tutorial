1b04b87932a59411595eb903a0ccd376cbbff4bf
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:22:36 +0530

    creating date in JS

--- JavaScript_intro/hitesh_sir/basics/08_dates_in_js.js
+++ JavaScript_intro/hitesh_sir/basics/08_dates_in_js.js
@@ -5,3 +5,11 @@ console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toISOString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 23);
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate2 = new Date("2023-01-21");
+console.log(myCreatedDate2);
+console.log(myCreatedDate2.toLocaleString());
