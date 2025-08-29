3b573328dc1a9f93b04c23a39cb34637bf664266
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:41:26 +0530

    arrays in JS

---
+++ JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
@@ -0,0 +1,5 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]; // Javascripts ke array resizable hote hai, we can add more elements, elements can be of different datatypes
+// Array is 0-based indexed in JS
+console.log(myArr[0]);
