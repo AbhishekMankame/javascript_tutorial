9adbdac485d4cf09cae79189a0a2907cccc3733d
abhishek <abhishekmankame962@gmail.com>
2025-08-29 19:11:20 +0530

    array from, is array update

--- JavaScript_intro/hitesh_sir/02_basics/02_arrays.js
+++ JavaScript_intro/hitesh_sir/02_basics/02_arrays.js
@@ -20,3 +20,7 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
+
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+console.log(Array.from({name: "Hitesh"})) // Interesting case: Here we need to specify kiska array banana hai
