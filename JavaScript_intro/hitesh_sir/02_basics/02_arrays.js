e21ff9a91d16825fd2769186d93ecb1fb6ec6c11
abhishek <abhishekmankame962@gmail.com>
2025-08-29 18:59:03 +0530

    array concatenation

--- JavaScript_intro/hitesh_sir/02_basics/02_arrays.js
+++ JavaScript_intro/hitesh_sir/02_basics/02_arrays.js
@@ -1,6 +1,13 @@
 const marvel_heros = ["Thor", "Iron man", "Spider man"];
 const dc_heros = ["superman", "flash", "batman"];
 
-marvel_heros.push(dc_heros); // yaha par arrays merge nahi hue hai, array ke anday array aagaya
-console.log(marvel_heros);
-console.log(marvel_heros.length);
+//marvel_heros.push(dc_heros); // yaha par arrays merge nahi hue hai, array ke anday array aagaya
+//console.log(marvel_heros);
+// console.log(marvel_heros.length);
+// console.log(marvel_heros[3]);
+// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[2][1]);
+
+// concat
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
