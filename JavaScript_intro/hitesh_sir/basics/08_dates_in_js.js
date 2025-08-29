588a1855dd2f5c051c20ce47c2df1240373257d6
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:15:22 +0530

    date in JS

---
+++ JavaScript_intro/hitesh_sir/basics/08_dates_in_js.js
@@ -0,0 +1,7 @@
+// Dates in JavaScript
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
