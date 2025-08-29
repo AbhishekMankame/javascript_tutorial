5a9fa40c4c76d7982f763ae9a142a0e938d4ce56
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:46:30 +0530

    push and pop in JS

--- JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
+++ JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
@@ -3,3 +3,12 @@
 const myArr = [0, 1, 2, 3, 4, 5]; // Javascripts ke array resizable hote hai, we can add more elements, elements can be of different datatypes
 // Array is 0-based indexed in JS
 console.log(myArr[0]);
+
+const myHeros = ["shaktiman", "nagaraj"];
+const myArr2 = new Array(1,2,3,4);
+
+// Array methods
+myArr.push(6);
+myArr.push(7);
+myArr.pop();
+console.log(myArr);
