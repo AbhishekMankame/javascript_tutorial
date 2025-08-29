c81ec634a132349443998eb249fc885204f09132
abhishek <abhishekmankame962@gmail.com>
2025-08-29 16:59:52 +0530

    math random concepts

--- JavaScript_intro/hitesh_sir/basics/07-nums&maths.js
+++ JavaScript_intro/hitesh_sir/basics/07-nums&maths.js
@@ -26,3 +26,9 @@ console.log(Math.sqrt(4));
 console.log(Math.min(4,3,6,7,8,1));
 console.log(Math.max(4,3,5,6,1,8,7));
 console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
