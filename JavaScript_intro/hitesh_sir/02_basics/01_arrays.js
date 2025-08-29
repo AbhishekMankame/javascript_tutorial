0aba47868568240a91c5c200d196dee06ebddd52
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:51:46 +0530

    some array functions

--- JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
+++ JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
@@ -6,9 +6,16 @@ console.log(myArr[0]);
 
 const myHeros = ["shaktiman", "nagaraj"];
 const myArr2 = new Array(1,2,3,4);
-
+myArr.unshift(8);
+myArr.shift();
+//myArr.shift();
 // Array methods
 myArr.push(6);
 myArr.push(7);
 myArr.pop();
 console.log(myArr);
+
+console.log(myArr.includes(2));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(2));
