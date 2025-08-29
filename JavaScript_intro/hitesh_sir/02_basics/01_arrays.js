5cf220538d9c33d59d610997400a1c39bff2aca6
abhishek <abhishekmankame962@gmail.com>
2025-08-29 17:59:03 +0530

    slice and splice in array is JS

--- JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
+++ JavaScript_intro/hitesh_sir/02_basics/01_arrays.js
@@ -19,3 +19,21 @@ console.log(myArr.includes(2));
 console.log(myArr.includes(9));
 console.log(myArr.indexOf(9));
 console.log(myArr.indexOf(2));
+
+const newArr =  myArr.join();
+console.log(myArr);
+console.log(newArr);
+console.log(typeof myArr);
+console.log(typeof newArr);
+
+// slice, splice
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+console.log("C",myArr);
+
+// Splice use karne ke baad, joh portion range mei tha, woh original array mei se nikal hi gaya hai
